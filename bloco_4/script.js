7e6de0692d5c9fbfcbafaa34dcaaa7f237728b08
matheus <matheus111904@hotmail.com>
2021-04-22 18:35:13 -0300

    exercicio 1 concluido

---
+++ bloco_4/script.js
@@ -0,0 +1,18 @@
+/* let a = 13
+let b = 23
+
+let soma = (a + b);
+  console.log(soma);
+
+let subtracao = (a - b);
+  console.log(subtracao);
+
+let multi = (a * b);
+  console.log(multi);
+
+let divisao = (a / b);
+  console.log(divisao);
+
+let modulo = (a % b);
+  console.log(modulo);
+*/
