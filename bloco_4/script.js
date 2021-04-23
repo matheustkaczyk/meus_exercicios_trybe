398bff6342323309ccedb824965a129b5b0a9014
matheus <matheus111904@hotmail.com>
2021-04-23 12:38:27 -0300

    exercicions concluidos

--- bloco_4/script.js
+++ bloco_4/script.js
@@ -122,7 +122,7 @@ if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0){
 }
 console.log(epar);
 */
-/* 8
+/* 9
 let numero1 = 2;
 let numero2 = 4;
 let numero3 = 7;
@@ -134,3 +134,45 @@ if (numero1 % 2 !== 0 || numero2 % 2 !== 0 || numero3 % 2 !== 0){
 }
 console.log(eimpar);
 */
+/* 10
+let custoProduto = 100;
+let valorVenda = 150;
+
+if (custoProduto >= 0 && valorVenda >= 0);{
+  let custoTotalProduto = custoProduto * 1.2;
+  let lucroTotal = (valorVenda - custoTotalProduto) * 1000;
+  console.log(lucroTotal);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
+*/
+let salarioBruto = 3000;
+let salarioBase = 0;
+let inss = 0;
+let ir = 0;
+
+if (salarioBruto <= 1556.94){
+  inss = (salarioBruto * 0.08);
+} else if (salarioBruto <= 2594.92) {
+  inss = (salarioBruto * 0.09);
+} else if (salarioBruto <= 5189.82) {
+  inss = (salarioBruto * 0.11);
+} else {
+  inss = 570.88;
+}
+
+salarioBase = salarioBruto - inss;
+
+if (salarioBase < 1903.98){
+  ir = 0;
+} else if (salarioBase <= 2826.65) {
+  ir = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase <= 3751.05){
+  ir = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68){
+  ir = (salarioBase * 0.225) - 636.13;
+} else {
+  ir = (salarioBase * 0.275) - 869.36;
+}
+let salarioLiquido = (salarioBase - ir);
+console.log("Salário: " + (salarioLiquido));
