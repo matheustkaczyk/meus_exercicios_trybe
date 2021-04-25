7f95cf52f5a6244b2976093d5467d2bc0449dd12
matheus <matheus111904@hotmail.com>
2021-04-25 11:19:52 -0300

    exercicio 6 concluido

--- bloco_4/script.js
+++ bloco_4/script.js
@@ -1,9 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorNumero = numbers[0];
+let impares = 0;
 
 for (let index = 0; index < numbers.length; index += 1){
-  if (numbers[index] > maiorNumero){
-    maiorNumero = numbers[index]
-  };
-  };
-  console.log(maiorNumero);
+  if (numbers[index] % 2 !== 0){
+    impares += 1;
+  }
+}
+if (impares === 0){
+  console.log("Não há números ímpares!");
+} else {
+  console.log(impares);
+}
