8e93eae0fc050f8aa6832370528aba8d93ad85bc
matheus <matheus111904@hotmail.com>
2021-04-25 10:57:08 -0300

    exercicio 3 concluido

--- bloco_4/script.js
+++ bloco_4/script.js
@@ -1,7 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media = 0;
 
 for (let index = 0; index < numbers.length; index += 1){
   soma += numbers[index];
+  media = soma / numbers.length
 };
-console.log(soma);
+console.log(`A soma é: ${soma}`);
+console.log(`A média é: ${media}`);
