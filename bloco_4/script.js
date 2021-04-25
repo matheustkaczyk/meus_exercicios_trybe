70bd7d4478c0e3e9005ac3a83abf328d8a544481
matheus <matheus111904@hotmail.com>
2021-04-25 11:00:00 -0300

    concluido exercicio 4

--- bloco_4/script.js
+++ bloco_4/script.js
@@ -5,6 +5,11 @@ let media = 0;
 for (let index = 0; index < numbers.length; index += 1){
   soma += numbers[index];
   media = soma / numbers.length
+  }
+  if (media >= 20) {
+    console.log("Número é maior que 20!")
+  } else {
+    console.log("Número é menor ou igual a 20!")
 };
 console.log(`A soma é: ${soma}`);
 console.log(`A média é: ${media}`);
