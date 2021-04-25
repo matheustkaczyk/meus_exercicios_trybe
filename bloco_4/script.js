6d97216f9d35dab06a6bbc28b68f4237343f1260
matheus <matheus111904@hotmail.com>
2021-04-25 11:50:04 -0300

    exercicio 9 concluido

--- bloco_4/script.js
+++ bloco_4/script.js
@@ -1,6 +1,9 @@
 let numeros = [];
-
+let divisao = 0;
 for (let index = 1; index <= 25; index += 1){
   numeros.push(index);
 }
-  console.log(numeros);
+  console.log(numeros)
+for (let index = 0; index < numeros.length; index += 1){
+  console.log(numeros[index] / 2);
+}
