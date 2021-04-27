178463dd877a1ba169200dbb52fa5b75257ef830
matheus <matheus111904@hotmail.com>
2021-04-27 18:51:39 -0300

    exercicio 5 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,10 +1,17 @@
-let info = {
+let info1 = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
-  info.recorrente = "Sim";
+  info1.recorrente = "Sim";
 
-  for (let key in info) {
-    console.log(info[key]);
-  }
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+}
+console.log(info1.personagem + " e " + info2.personagem);
+console.log(info1.origem + " e " + info2.origem);
+console.log(info1.nota + " e " + info2.nota);
