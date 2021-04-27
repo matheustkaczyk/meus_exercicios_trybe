6c56ac3838cede921af122a2bba50b40d87aa6f8
matheus <matheus111904@hotmail.com>
2021-04-27 18:44:38 -0300

    exercicio 2 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -3,4 +3,5 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
-  console.log("Bem-vinda, " + info.personagem);
+  info.recorrente = "Sim";
+  console.log(info);
