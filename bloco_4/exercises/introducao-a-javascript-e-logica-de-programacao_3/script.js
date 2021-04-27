348eda4b6110d5ac46fb1a76e6e451578689562f
matheus <matheus111904@hotmail.com>
2021-04-27 08:31:18 -0300

    exercicio 1 concluido

---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/script.js
@@ -0,0 +1,10 @@
+let n = 5;
+let asterisco = '*';
+let linha = '';
+
+for (let linhaIndex = 0; linhaIndex < n; linhaIndex += 1) {
+  linha = linha + asterisco;
+};
+for (let linhaIndex = 0; linhaIndex < n; linhaIndex += 1){
+    console.log(linha);
+}
--- bloco_4/script.js
+++
@@ -1,9 +0,0 @@
-let numeros = [];
-let divisao = 0;
-for (let index = 1; index <= 25; index += 1){
-  numeros.push(index);
-}
-  console.log(numeros)
-for (let index = 0; index < numeros.length; index += 1){
-  console.log(numeros[index] / 2);
-}
