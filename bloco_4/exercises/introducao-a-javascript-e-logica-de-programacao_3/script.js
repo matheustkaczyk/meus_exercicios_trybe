315a5c7e8b8d14f8ddc3e3b8a90dafebc6aa2ea0
matheus <matheus111904@hotmail.com>
2021-04-27 09:19:15 -0300

    exercicio 03 concluido

--- bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/script.js
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/script.js
@@ -10,6 +10,7 @@ for (let linhaIndex = 0; linhaIndex < n; linhaIndex += 1){
     console.log(linha);
 }
 */
+/* 02
 let n = 5;
 let asterisco = "*";
 let linha = "";
@@ -18,3 +19,23 @@ for (let index = 0; index < n; index += 1){
   linha = linha + asterisco;
   console.log(linha);
 }
+*/
+/* 03
+let n = 5;
+let asterisco = '*';
+let linha = '';
+let posicao = n;
+
+for (let index = 0; index < n; index += 1) {
+  for (let colunaindex = 0; colunaindex <= n; colunaindex += 1){
+    if (colunaindex < posicao){
+      linha = linha + " ";
+    } else {
+      linha = linha + asterisco;
+    }
+  }
+  console.log(linha);
+  linha = "";
+  posicao -= 1;
+}
+*/
