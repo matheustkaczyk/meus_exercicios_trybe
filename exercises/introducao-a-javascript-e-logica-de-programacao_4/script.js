fd4cc10c02f6f3ccc00c133d7bbdbcbd4361c5de
matheus <matheus111904@hotmail.com>
2021-04-27 19:21:55 -0300

    funcao 1 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,17 +1,12 @@
-let info1 = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-  info1.recorrente = "Sim";
 
-
-let info2 = {
-    personagem: "Tio Patinhas",
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: "O último MacPatinhas",
-    recorrente: "Sim"
-}
-console.log(info1.personagem + " e " + info2.personagem);
-console.log(info1.origem + " e " + info2.origem);
-console.log(info1.nota + " e " + info2.nota);
+function verificaPalindrome(palavra) {
+    let arrayLetras = palavra.split('');
+    let isPalindrome = true;
+    for (let index in arrayLetras) {
+      if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+        isPalindrome = false;
+      }
+    }
+    return isPalindrome;
+  }
+  console.log(verificaPalindrome("tenet"))
