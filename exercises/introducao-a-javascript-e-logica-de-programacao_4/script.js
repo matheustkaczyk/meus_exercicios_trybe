09a57ea3b3cedc64fe99a6278412b1010e80963f
matheus <matheus111904@hotmail.com>
2021-04-28 09:14:40 -0300

    exercicio 4 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,11 +1,10 @@
-function indiceDoMaior(numeros) {
-    let indiceMenor = 0;
-    for (let indice in numeros) {
-      if (numeros[indiceMenor] > numeros[indice]) {
-        indiceMenor = indice;
-      }
+function mCarac(nomes){
+    let maiorNome = nomes[0];
+    for (let index in nomes){
+        if (maiorNome.length < nomes[index].length){
+            maiorNome = nomes[index];
     }
-    return indiceMenor;
-  }
-  
-  console.log(indiceDoMaior([2, 4, 6, 7, 10, 0, -3]));
+}
+return maiorNome;
+}
+console.log(mCarac(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
