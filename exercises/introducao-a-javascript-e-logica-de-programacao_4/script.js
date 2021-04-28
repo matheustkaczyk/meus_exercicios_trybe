60ab2d4ee0bfb7cd243d86330581f46d9378efaf
matheus <matheus111904@hotmail.com>
2021-04-28 09:51:42 -0300

    exercicio 5 analisado

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,10 +1,20 @@
-function mCarac(nomes){
-    let maiorNome = nomes[0];
-    for (let index in nomes){
-        if (maiorNome.length < nomes[index].length){
-            maiorNome = nomes[index];
+function maisRepete(numeros){
+    let numeroRepete = 0;
+    let contagemNumero = 0;
+    let indexNumRep = 0;
+    for (let index in numeros){
+        let verificacaoNumero = numeros[index];
+        for (let index2 in numeros){
+        if (verificacaoNumero === numeros[index2]){
+            contagemNumero += 1;
+        }
+        }
+    if (contagemNumero > numeroRepete){
+        numeroRepete = contagemNumero;
+        indexNumRep = index;
     }
+    contagemNumero = 0;
+    }
+return numeros[indexNumRep];
 }
-return maiorNome;
-}
-console.log(mCarac(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+console.log(maisRepete([4, 3, 3, 5, 5, 6, 8, 8, 8, 8, 8]));
