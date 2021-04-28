ea077b685874e7da78aaea36acb954ad94abf285
matheus <matheus111904@hotmail.com>
2021-04-28 10:06:59 -0300

    exercicio 6 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,20 +1,8 @@
-function maisRepete(numeros){
-    let numeroRepete = 0;
-    let contagemNumero = 0;
-    let indexNumRep = 0;
-    for (let index in numeros){
-        let verificacaoNumero = numeros[index];
-        for (let index2 in numeros){
-        if (verificacaoNumero === numeros[index2]){
-            contagemNumero += 1;
-        }
-        }
-    if (contagemNumero > numeroRepete){
-        numeroRepete = contagemNumero;
-        indexNumRep = index;
+function soma(n){
+    let total = 0;
+    for (let index = 0; index <= n; index ++) {
+        total += index;
     }
-    contagemNumero = 0;
-    }
-return numeros[indexNumRep];
+    return total;
 }
-console.log(maisRepete([4, 3, 3, 5, 5, 6, 8, 8, 8, 8, 8]));
+console.log(soma(20));
