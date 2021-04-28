471e784d50f9da0503185c5082b93315a898882e
matheus <matheus111904@hotmail.com>
2021-04-28 08:58:36 -0300

    exercicio 2 entendido

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -1,12 +1,12 @@
-
-function verificaPalindrome(palavra) {
-    let arrayLetras = palavra.split('');
-    let isPalindrome = true;
-    for (let index in arrayLetras) {
-      if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
-        isPalindrome = false;
+function indiceDoMaior(numeros) {
+    let indiceMaior = 0;
+    for (let indice in numeros) {
+      if (numeros[indiceMaior] < numeros[indice]) {
+        indiceMaior = indice;
       }
     }
-    return isPalindrome;
+    return indiceMaior;
   }
-  console.log(verificaPalindrome("tenet"))
+  
+  console.log(indiceDoMaior([8, 12, 6, 7, 16, 24]));
+  
